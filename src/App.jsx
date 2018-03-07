7aca6b33f7d8a9c00248f98d0c356f8dde7a5add
Lucas Franco <git@lucasfranco.com.br>
2018-03-07 17:20:52 -0300

    Structure and header+grid basic components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Header from './components/Header'
+import Grid from './components/Grid'
+
+const App = () => (
+  <React.Fragment>
+    <Header />
+    <Grid />
+  </React.Fragment>
+)
+
+export default App
---
+++ src/components/Grid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Grid = () => (
+  <section>
+    <p>Grid</p>
+  </section>
+)
+
+export default Grid
