75c27feccd1aa50361297be5e4f9c674c6482cc9
Lucas Franco <lucas@lucasfranco.com.br>
2018-03-08 08:09:29 -0300

    The grid with cssgrid and some styles

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledSection = styled.section`
+  position: relative;
+  padding-top: 50px;
+`
+
+const StyledGrid = styled.div`
+  position: absolute;
+  left: 50%;
+  width: 70vh;
+  height: 70vh;
+  background-color: #d1d8e0;
+  transform: translateX(-50%);
+  display: grid;
+  grid-template-columns: repeat(9, 11.11% 0);
+  grid-template-rows: repeat(9, 11.11% 0);
+`
+
+const Item = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+`
 
 const Grid = () => (
-  <section>
-    <p>Grid</p>
-  </section>
+  <StyledSection>
+    <StyledGrid>
+      <Item>1</Item>
+    </StyledGrid>
+  </StyledSection>
 )
 
 export default Grid
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,10 @@ import styled from 'styled-components'
 
 const StyledHeader = styled.header`
   padding: 20px;
-  color: #555;
+  color: #fff;
   width: 100%;
   text-align: center;
-  background-color: #DCDCDC;
-
+  background-color: #8854d0;
 `
 
 const Header = () => (
