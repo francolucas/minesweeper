0b08049ce56087d9bdb4106c9a76cecc18a18e61
Lucas Franco <git@lucasfranco.com.br>
2018-03-13 17:44:31 -0300

    Get around items when click

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -6,21 +6,21 @@ import Item from './Item'
 const StyledSection = styled.section`
   position: relative;
   padding-top: 50px;
-`
 
-const StyledGrid = styled.div`
-  position: absolute;
-  left: 50%;
-  width: 70vh;
-  height: 70vh;
-  max-width: 90vw;
-  max-height: 90vw;
-  background-color: #d1d8e0;
-  transform: translateX(-50%);
-  display: grid;
-  grid-template: repeat(9, calc(100% / 9 - 1px)) / repeat(9, calc(100% / 9 - 1px));
-  grid-gap: 1px;
-  padding: 1px 0 0 1px;
+  > div {
+    position: absolute;
+    left: 50%;
+    width: 70vh;
+    height: 70vh;
+    max-width: 90vw;
+    max-height: 90vw;
+    background-color: #d1d8e0;
+    transform: translateX(-50%);
+    display: grid;
+    grid-template: repeat(9, calc(100% / 9 - 1px)) / repeat(9, calc(100% / 9 - 1px));
+    grid-gap: 1px;
+    padding: 1px 0 0 1px;
+  }
 `
 
 const arr = []
@@ -30,11 +30,11 @@ for (let i = 0; i < 81; i++) {
 
 const Grid = () => (
   <StyledSection>
-    <StyledGrid>
+    <div>
       {
-        arr.map(v => <Item key={v} value={v} />)
+        arr.map((v, index) => <Item key={index} index={index} />)
       }
-    </StyledGrid>
+    </div>
   </StyledSection>
 )
 
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 
 const StyledItem = styled.span`
@@ -7,10 +7,49 @@ const StyledItem = styled.span`
   align-items: center;
   background-color: #fff;
   font-size: 4vh;
+  position: relative;
+
+  &:after {
+    content: attr(data-title);
+    font-size: 1vh;
+    color: #d1d8e0;
+    position: absolute;
+    top: .5vh;
+    left: .5vh;
+  }
 `
 
-const handleClick = value => alert(value)
+class Item extends Component {
+  constructor(props) {
+    super(props)
 
-const Item = ({value}) =><StyledItem onClick={() => handleClick(value)}>{value}</StyledItem>
+    this.state = {
+      currentIndex: [Math.floor(props.index / 9), Math.round(props.index % 9)]
+    }
+  }
+  handleClick = () => {
+    const { currentIndex } = this.state
+    const around = [
+      [currentIndex[0] - 1, currentIndex[1]],
+      [currentIndex[0] - 1, currentIndex[1] + 1],
+      [currentIndex[0], currentIndex[1] + 1],
+      [currentIndex[0] + 1, currentIndex[1] + 1],
+      [currentIndex[0] + 1, currentIndex[1]],
+      [currentIndex[0] + 1, currentIndex[1] - 1],
+      [currentIndex[0], currentIndex[1] - 1],
+      [currentIndex[0] - 1, currentIndex[1] - 1],
+    ].filter(index =>
+      index[0] >= 0 && index[0] < 9 && index[1] >= 0 && index[1] < 9
+    )
+
+    // Update the around items
+  }
+
+  render() {
+    return (
+      <StyledItem onClick={this.handleClick} data-title={this.props.index + 1} />
+    )
+  }
+}
 
 export default Item
